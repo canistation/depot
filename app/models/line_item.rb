e30f267d4932ee5e20e77c9aae62f5198bdd7fb7
WolfLee <liyuan0228@gmail.com>
2012-12-04 23:40:09 +0800

    Finishing Cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,10 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id
+  attr_accessible :cart_id, :product_id, :quantity
   #attr_accessible :product
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+    product.price * quantity
+  end
 end
