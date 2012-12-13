f1ca515442ca75ab6f21ab0ef8cf4c1a70618dec
WolfLee <liyuan0228@gmail.com>
2012-12-13 22:34:35 +0800

    Internationalization

--- config/routes.rb
+++ config/routes.rb
@@ -13,21 +13,23 @@ Depot::Application.routes.draw do
 
   #get "sessions/destroy"
 
-  resources :users
+  scope '(:locale)' do
+    resources :users
 
-  resources :orders
+    resources :orders
 
-  resources :line_items
+    resources :line_items
 
-  resources :carts
+    resources :carts
 
-  get "store/index"
+    get "store/index"
 
-  resources :products do
-    get :who_bought, on: :member
+    resources :products do
+      get :who_bought, on: :member
+    end
+
+    root :to => 'store#index', as: 'store'
   end
-
-  root :to => 'store#index', as: 'store'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
